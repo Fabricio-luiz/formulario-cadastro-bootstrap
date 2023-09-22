8b7112ec2c74af327964ef7319a85f189de60d92
Fabricio <fabricioflr@gmail.com>
2023-09-22 13:17:58 -0300

    inital commit

---
+++ main.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(xx) xxxxx-xxxx'
+    })
+
+    })
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true 
+            },
+            aceitoTermos: {
+                required: true 
+            },
+        },
+
+        messages: {
+            nome:'<span class="message-error">Por favor, insira seu nome</span>',
+            email:'<span class="message-error">Por favor, insira seu e-mail</span>',
+            telefone:'<span class="message-error">Por favor, insira seu telefone</span>',
+            mensagem: '<span class="message-error">Este campo é obrigatório</span>',
+            aceitoTermos:'<span class="message-error">Este campo é obrigatório</span>',
+        },
+        submitHandler: function(form) {
+            alert('Formulário cadastrado')
+            form.reset();
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+            /* alert(`Existe ${camposIncorretos} campo(s) incorreto(s)`)*/
+            }
+        }
+    })
